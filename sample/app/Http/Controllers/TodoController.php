2cf0a38d862647a22d176331803f25d7760e82c7
mamemaru00 <k.shouhei0711@gmail.com>
2022-10-09 01:24:26 +0900

    API設定

--- sample/app/Http/Controllers/TodoController.php
+++ sample/app/Http/Controllers/TodoController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Todo;
 
 class TodoController extends Controller
 {
@@ -13,18 +14,10 @@ class TodoController extends Controller
      */
     public function index()
     {
-        //
-    }
+        $users = User::all();
+        if ($users->isEmpty()) return response()->json(null, 404);
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
+        return response()->json(User::all(), 200);
     }
 
     /**
@@ -35,7 +28,16 @@ class TodoController extends Controller
      */
     public function show($id)
     {
-        //
+        $user = User::find($id);
+        if (is_null($user)) return response()->json(null, 404);
+
+        return response()->json([
+            'id'         => $user->id,
+            'name'       => $user->name,
+            'email'      => $user->email,
+            'created_at' => $user->created_at,
+            'updated_at' => $user->updated_at,
+        ], 200);
     }
 
     /**
@@ -47,7 +49,14 @@ class TodoController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $user = User::find($id);
+        if (is_null($user)) return response()->json(null, 404);
+
+        $user->name = $request->input('name');
+        $user->password = $request->input('password');
+        $user->save();
+
+        return response()->json(null, 204);
     }
 
     /**
@@ -58,6 +67,10 @@ class TodoController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $user = User::find($id);
+        if (is_null($user)) return response()->json(null, 404);
+
+        User::find($id)->delete();
+        return response()->json(null, 204);
     }
 }
