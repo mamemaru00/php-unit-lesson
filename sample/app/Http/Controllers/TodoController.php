3752cf1150eaf1cffc15d82529b767a53d52d0ff
mamemaru00 <k.shouhei0711@gmail.com>
2022-10-11 22:53:30 +0900

    テスト

--- sample/app/Http/Controllers/TodoController.php
+++ sample/app/Http/Controllers/TodoController.php
@@ -3,55 +3,37 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Todo;
 use App\Models\User;
+use App\Http\Controllers\Controller;
 
 class TodoController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
-        $users = Todo::all();
-        if ($users->isEmpty()) return response()->json(null, 404);
+        $users = User::all();
+        if($users->isEmpty()) return response()->json(null,404);
 
-        return response()->json(Todo::all(), 200);
+        return response()->json(User::all(), 200);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function show($id)
     {
-        $user = Todo::find($id);
-        if (is_null($user)) return response()->json(null, 404);
+        $user = User::find($id);
+        if(is_null($user)) return response()->json(null, 404);
 
         return response()->json([
-            'id'         => $user->id,
-            'name'       => $user->name,
-            'email'      => $user->email,
-            'created_at' => $user->created_at,
-            'updated_at' => $user->updated_at,
+            'id' =>$user->id,
+            'name' =>$user->name,
+            'email' =>$user->email,
+            'created_at' =>$user->created_at,
+            'update_at' =>$user->update_at,
         ], 200);
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, $id)
     {
-        $user = Todo::find($id);
-        if (is_null($user)) return response()->json(null, 404);
+        $user = User::find($id);
+        if(is_null($user)) return response()->json(null, 404);
 
         $user->name = $request->input('name');
         $user->password = $request->input('password');
@@ -60,18 +42,12 @@ class TodoController extends Controller
         return response()->json(null, 204);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function destroy($id)
     {
-        $user = Todo::find($id);
-        if (is_null($user)) return response()->json(null, 404);
+      $user = User::find($id);
+      if(is_null($user)) return response()->json(null, 404);
 
-        Todo::find($id)->delete();
-        return response()->json(null, 204);
+      User::find($id);
+      return response()->json(null, 204);
     }
 }
--- sample/database/factories/TodoFactory.php
+++ sample/database/factories/TodoFactory.php
@@ -2,11 +2,11 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
-use App\Models\Todo;
+use App\Models\User;
 use Faker\Generator as Faker;
 use Illuminate\Support\Str;
 
-$factory->define(Todo::class, function (Faker $faker) {
+$factory->define(User::class, function (Faker $faker) {
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
